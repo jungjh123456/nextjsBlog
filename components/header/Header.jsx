95f37a9cf75dc13e3e7e4fa489ffa2690364e624
jungjh123456 <jungjh123456@naver.com>
2021-03-30 17:43:18 +0900

    feat: React file

---
+++ components/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { HeaderStyle } from './HeaderStyle';
+
+const Header = () => {
+	return <HeaderStyle></HeaderStyle>;
+};
+
+export default Header;
---
+++ components/header/HeaderStyle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HeaderStyle = styled.header`
+	width: 100%;
+	background-color: red;
+`;
+
+export const UlStyle = styled.header`
+	width: 100%;
+	background-color: red;
+`;
